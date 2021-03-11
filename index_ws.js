41d8f1d20e2d4cabc2c3f861871568500ee67952
takashun <s1832087py@s.chibakoudai.jp>
2021-03-12 00:09:41 +0900

    ゲームの失敗条件の修正

--- index_ws.js
+++ index_ws.js
@@ -116,7 +116,7 @@ wss.on('connection', (ws, req) => {
       }
 
       if(json.progType === "play") {
-        if(canPlay(roomObject, roomObject.gameTurnIndex)) {
+        if(roomObject.players[roomObject.gameTurnIndex].plays >= roomObject.minPlays || canPlay(roomObject, roomObject.gameTurnIndex)) {
           roomObject.players[roomObject.gameTurnIndex].plays++
           sendToPlayers("game-update", wss.clients, roomObject.roomId, {roomObject: roomObject, updateType: "update"})
         } else {
